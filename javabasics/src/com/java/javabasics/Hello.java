edd59a35490dd5e11ccea0301f3d90faad490e35
renukachuttugulla3-ship-it <renukachuttugulla3@gmail.com>
2025-08-23 10:53:54 +0530

    code commit
    code commit

---
+++ javabasics/src/com/java/javabasics/Hello.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.java.javabasics;
+
+/**
+ * 
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
