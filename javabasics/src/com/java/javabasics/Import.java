09073a734a5612d0c7b23e10b92cc903fb2d12f2
renukachuttugulla3-ship-it <renukachuttugulla3@gmail.com>
2025-08-23 18:19:08 +0530

    Create Import.java

---
+++ javabasics/src/com/java/javabasics/Import.java
@@ -0,0 +1,13 @@
+
+package com.java.javabasics;
+
+public class Import {
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		System.out.println("hello java");
+
+	}
+
+}
