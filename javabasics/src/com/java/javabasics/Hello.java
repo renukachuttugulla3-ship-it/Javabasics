144c0337840479a230bfad0ce7e153effcfd7093
renukachuttugulla3-ship-it <renukachuttugulla3@gmail.com>
2025-08-25 16:57:24 +0530

    commit code

--- javabasics/src/com/java/javabasics/Hello.java
+++ javabasics/src/com/java/javabasics/Hello.java
@@ -1,21 +1,10 @@
-/**
- * 
- */
 package com.java.javabasics;
 
-/**
- * 
- */
 public class Hello {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		
-		// TODO Auto-generated method stub
+	
+	public static void main(String[]args)
+	{
 		System.out.println("hello world");
-
 	}
 
 }
--- javabasics/src/com/java/javabasics/Tiger.java
+++ javabasics/src/com/java/javabasics/Tiger.java
@@ -1,12 +1,9 @@
-
 package com.java.javabasics;
 
-public class Import {
+public class Tiger {
+
 	public static void main(String[] args) {
-		
 		// TODO Auto-generated method stub
-		System.out.println("hello world");
-		System.out.println("hello java");
 
 	}
 
